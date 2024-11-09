a039b1e6398bfa73b5f2442cb2c4fdb716ec1f07
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2024-11-08 19:53:15 -0600

    Update gitlab.py

--- gnome/gitlab.py
+++ gnome/gitlab.py
@@ -11,6 +11,8 @@ def download_tarball_from_gitlab(domain, namespace, repo_name, version, extract_
     urls = [
         f"https://{domain}/{namespace}/{repo_name}/-/releases/{version}/downloads/{repo_name}-{version}.tar.xz",
         f"https://{domain}/{namespace}/{repo_name}/-/releases/v{version}/downloads/{repo_name}-v{version}.tar.xz"
+        f"https://{domain}/{namespace}/{repo_name}/-/releases/{version}/archive/{repo_name}-{version}.tar.xz",
+        f"https://{domain}/{namespace}/{repo_name}/-/releases/v{version}/archive/{repo_name}-v{version}.tar.xz"
     ]
 
     # Construct the filename and path where the tarball will be saved
