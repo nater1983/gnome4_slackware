edc59823d27083fa0463e9b918a0fc6453892be9
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2025-04-18 13:30:50 -0500

    Update gnome-dictionary.sh

--- gnome/gnome-dictionary.sh
+++ gnome/gnome-dictionary.sh
@@ -112,7 +112,7 @@ for LOC in $MODS; do
 
   # Get the latest commit date and hash
   cd ${LOC}-temp || continue
-  COMMIT_INFO=$(grep -Po "version:\s*'\K[0-9]+\.[0-9]+\.[0-9]+" meson.build)
+  COMMIT_INFO=$(grep -Po "version:\s*'\K[^']+" meson.build)
   cd ..
 
   # Rename the directory with commit info
